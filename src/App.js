6ba4636b0869b513c33c678a30baf3af1c7cf134
Alexis Michia <alexi890@outlook.com>
2023-12-12 10:51:01 -0300

    actu

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Header from './components/Header/Header';
 import Contact from './components/Contact';*/
 import Footer from './components/Footer/Footer'; 
 import Home from './components/Home/Home';
+
+
 function App() {
   return (
     <Router>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,44 +1,36 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 
 const Header = () => {
   return (
-    <header>
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <Link className="navbar-brand" to="/" style={{ fontWeight: 'bold', textShadow: '2px 2px 2px rgba(0, 0, 0, 0.5)' }}>
-          Brandboosters
-        </Link>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav ml-auto">
-            <li className="nav-item">
-              <Link className="nav-link" to="/services">Servicios</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/portfolio">Portafolio</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/contact">Contacto</Link>
-            </li>
-          </ul>
-        </div>
-      </nav>
+    <Navbar bg="dark" expand="lg" variant="dark">
+      <Navbar.Brand as={Link} to="/" style={{ fontWeight: 'bold', textShadow: '2px 2px 2px rgba(0, 0, 0, 0.5)' }}>
+        Brandboosters
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="navbarNav" />
+      <Navbar.Collapse id="navbarNav">
+        <Nav className="ml-auto">
+          <Nav.Link as={Link} to="/services">Servicios</Nav.Link>
+          <Nav.Link as={Link} to="/portfolio">Portafolio</Nav.Link>
+          <Nav.Link as={Link} to="/contact">Contacto</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
       <style>
         {`
           .nav-link {
-            transition: color 0.3s; /* Agregamos una transición para suavizar el cambio de color */
-            font-family: 'cursiva'; /* Cambia 'cursiva' por el nombre de tu tipo de letra cursiva o moderna */
+            transition: color 0.3s;
+            font-family: 'cursiva';
           }
 
           .nav-link:hover {
-            color: #ffff00; /* Amarillo fosforescente */
-            border-bottom: 4px solid #ffff00; /* Línea amarilla gruesa en la parte inferior */
+            color: #ffff00;
+            border-bottom: 4px solid #ffff00;
           }
         `}
       </style>
-    </header>
+    </Navbar>
   );
 }
 
